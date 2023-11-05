d89c888ffeb89e5050ab3721f144c2ac2affd005
Weslley <weslley.c.j.4@gmail.com>
2023-11-05 16:14:17 -0300

    Colocado parte DetailView

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path
-from produtos.views import ProductListView
+from produtos.views import ProductListView, ProductDetailView
+
+app_name = 'produtos'
 
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='produtos'),
+    path('', ProductListView.as_view(), name='listagem'),
+    path('detalhes-produto/<int:id>', ProductDetailView.as_view(), name='detalhes'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -7,3 +7,10 @@ class ProductListView(ListView):
     model = Products
     context_object_name = 'produtos'
     template_name = 'produtos/product_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Products
+    context_object_name = 'produto'
+    template_name = 'produtos/detalhar.html'
+    pk_url_kwarg = 'id'
